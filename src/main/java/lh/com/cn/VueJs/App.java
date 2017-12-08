7ca5c92adec3897925ff417b1012d05118d10177
lh <lh_1215077166@sina.cn>
2017-12-08 20:46:13 +0800

    环境搭建

---
+++ src/main/java/lh/com/cn/VueJs/App.java
@@ -0,0 +1,13 @@
+package lh.com.cn.VueJs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
